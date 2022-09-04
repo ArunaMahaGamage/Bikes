8cf5f3be499c8f02a197c0915f6dc23b3ebb27ae
ArunaFortuna <91108844+ArunaFortuna@users.noreply.github.com>
2022-09-04 11:50:57 +0530

    Fix API call issue

--- app/src/main/java/com/example/bikes/SplashActivity.kt
+++ app/src/main/java/com/example/bikes/SplashActivity.kt
@@ -4,13 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.ImageView
 
 class SplashActivity : AppCompatActivity() {
+
+    lateinit var iv_background: ImageView;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({ startApp() },2000)
+//        Handler().postDelayed({ startApp() },2000)
+
+        iv_background = findViewById(R.id.imageView2);
+        iv_background.setOnClickListener {
+            startApp()
+        }
     }
 
     private fun startApp() {
